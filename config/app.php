10bc141cce657a1f43d93ab73b59c0ae27562a05
python takuma <go.ahead.mabuhay@gmail.com>
2019-02-25 21:14:05 +0900

    add HTTPS

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'name' => env('APP_NAME', 'Foodsplash'),
+    'name' => env('APP_NAME', 'Foodsplash1'),
     /*
     |--------------------------------------------------------------------------
     | Application Environment
